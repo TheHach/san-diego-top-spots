8a3bd915a389bbf891b58456275618c29a94b9db
Hunny Hach <hunny.ny.hach@gmail.com>
2020-01-21 15:18:09 -0800

    Completed top spots

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function(){
 // write your code here
-
+    $.getJSON("data.json", function (data) {
+    $.each(data, function (key, value) {
+        $('#dataTable').append(`<tr>
+        <td>${value.name}</td>
+        <td>${value.description}</td>
+        <td><a href="https://www.google.com/maps?q=" + ${value.location}>Google Maps</a>
+        </td></tr>`);
+        });
+    });
 });
